29a01fc52142bb24fff84b4cd63fa8634cc42978
Karan <Karansinghallen17@gmail.com>
2023-02-23 13:57:21 +0530

    CRUD USING PRISMA is done except update query...

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -1,26 +1,52 @@
-import {PrismaClient} from "@prisma/client"
-const prisma= new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default async function handler(req,res) {
-    if(req.method==="GET"){
-        const main = async () =>{
-            const profiles = await prisma.userlogin.findMany()
-            return profiles
-        }
-        try {
-            const profiles = await main(); // Wait for the main() function to complete and return the data
-            res.status(200).json(profiles); // Send the response with the retrieved data
-          } catch (e) {
-            console.log("error : ", e.message);
-          } finally {
-            await prisma.$disconnect();
-          }
-        }
-
-
-
-
-
-
-
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const main = async () => {
+      const profiles = await prisma.userlogin.findMany();
+      return profiles;
+    };
+    try {
+      const profiles = await main(); // Wait for the main() function to complete and return the data
+      res.status(200).json(profiles); // Send the response with the retrieved data
+    } catch (e) {
+      console.log("error : ", e.message);
+    } finally {
+      await prisma.$disconnect();
     }
+  }
+  if (req.method === "POST") {
+    // console.log("post called");
+    try {
+      const { username, password } = req.body;
+      const profile = await prisma.userlogin.create({
+        data: {
+          username,
+          password,
+        },
+      });
+      res.status(200).json({ name: "post called ho gayi" });
+    } catch (e) {
+      console.log("error aa gaya : ", e.message);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+
+  if (req.method === "DELETE") {
+    try {
+      const id = req.query.id;
+      await prisma.userlogin.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      res.status(200).json({ name: "Delete query chl gyi" });
+    } catch (e) {
+      console.log("error aa gaya : ", e.message);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,93 @@
+import { Inter } from "@next/font/google";
+import useSWR from "swr";
+import React, { useState } from "react";
 
-import { Inter } from '@next/font/google'
-import useSWR from 'swr';
-const inter = Inter({ subsets: ['latin'] })
+let name, value;
 
 export default function Home() {
-  const fetcher = async ()=>{
-    const resp = await fetch("api/profile")
-    const data = await resp.json()
-    return data
-  }
-
-  const getData = () =>{
-    
-  }
-  const {data,error} = useSWR("dash",fetcher);
-  if(error) return "Error aa gaya bhailog"
-  if(!data) return <h1>Loading...</h1>
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
+    console.log(user);
+  };
+  const sendData = async () => {
+    fetch("api/profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        console.log(resp);
+      });
+  };
+  const handleDelete = async (id) => {
+    console.log(id);
+    try {
+      await fetch(`api/profile?id=${id}`, {
+        method: "DELETE",
+      });
+    } catch (e) {
+      console.log("error : ", e.message);
+    }
+  };
+  const fetcher = async () => {
+    const resp = await fetch("api/profile");
+    const data = await resp.json();
+    return data;
+  };
+  const { data, error } = useSWR("dash", fetcher);
+  if (error) return "Error aa gaya bhailog";
+  if (!data) return <h1>Loading...</h1>;
   return (
     <>
       <div>
-        <h2>username | password</h2>
-        {data.map(ele=>{
-          return (<h2 key={ele.id}>{ele.username} | {ele.password}</h2>);
+        <h2>id → username | password</h2>
+        {data.map((ele) => {
+          return (
+            <h2 key={ele.id}>
+              {ele.id} → {ele.username} | {ele.password}{" "}
+              <button
+                onClick={() => {
+                  handleDelete(ele.id);
+                }}
+              >
+                Delete
+              </button>
+            </h2>
+          );
         })}
       </div>
+      <div>
+        <b>username : </b>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          onChange={handleInput}
+        />
+        <br />
+        <br />
+        <b>password : </b>
+        <input
+          id="password"
+          name="password"
+          type="text"
+          onChange={handleInput}
+        />
+        <br />
+        <br />
+        <button onClick={sendData}> submit</button>
+      </div>
     </>
-  )
+  );
 }
